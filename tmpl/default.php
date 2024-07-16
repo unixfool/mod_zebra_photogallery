845814a1912836ba367a60de9c3bd252ec653522
y2k <66226195+unixfool@users.noreply.github.com>
2024-07-16 22:41:30 +0200

    Add files via upload

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,6 +1,24 @@
 <?php defined('_JEXEC') or die; ?>
-
 <?php 
+/**
+ * This file is part of the Zebra Photogallery module.
+ *
+ * @license GPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+ 
 $enableFancybox = $params->get('enable_fancybox', 1);
 
 if ($enableFancybox) : ?>
